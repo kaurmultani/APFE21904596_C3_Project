e7ff4a8cb78bb201c2c69ffe9e272a631797313f
kaurmultani <47890763+kaurmultani@users.noreply.github.com>
2021-09-03 12:18:48 +0200

    find Restaurant By Name

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -6,11 +6,14 @@ public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
     public Restaurant findRestaurantByName(String restaurantName){
+         for(Restaurant restaurant: restaurants) {
+            if(restaurant.getName().equals(restaurantName))
+                return restaurant;
+        }
         return null;
         //DELETE ABOVE STATEMENT AND WRITE CODE HERE
     }
-
-
+    
     public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
         Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
         restaurants.add(newRestaurant);
